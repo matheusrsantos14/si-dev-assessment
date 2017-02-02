c8d701e1d9d3829a5006aa3b46bcd19f77420bdf
Matheus <matheusrsantos14@gmail.com>
2017-02-02 18:29:49 +0000

    Finishing Task 6

--- Gemfile
+++ Gemfile
@@ -53,3 +53,8 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 ruby "2.3.0"
+
+
+gem 'twilio-ruby'
+
+gem 'figaro'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,18 @@ class PagesController < ApplicationController
   end
 
   def sms
+    require 'twilio-ruby'
+    
+   # ENV["ACCOUNT_SID"] # Your Account SID from www.twilio.com/console
+   # ENV["AUTH_TOKEN"]   # Your Auth Token from www.twilio.com/console
+    
+    @client = Twilio::REST::Client.new ENV["ACCOUNT_SID"], ENV["AUTH_TOKEN"] 
+    
+    message = @client.account.messages.create(:body => params[:message],
+        :to => "7818598110",    # Replace with your phone number
+        :from => "6176827977")  # Replace with your Twilio number
+    
+    puts message.sid
   end
+
 end
